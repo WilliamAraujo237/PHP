2606f730d8bc73d6aef131b35f14b26e58ca3366
WilliamAraujo237 <william237.araujo@gmail.com>
2021-11-03 12:53:05 -0300

    Create form.php
    formulário com php

---
+++ form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulario com php</title>
+</head>
+<body>
+    <?php
+
+    $nome=$_GET['nome'];
+    $sobre_nome=$_GET['sobre-nome'];
+    $idade=$_GET['ano'];
+
+    if (date('Y')- $idade >= 63) {
+        $pessoa="já não é obrigado a votar";
+    }elseif(date('Y') - $idade < 18){
+        $pessoa="Ainda não pode votar";
+    }elseif (date('Y') - $idade > 18) {
+        $pessoa="já é obridao a votar";
+    }
+
+    echo"olá $nome $sobre_nome você $pessoa"
+    ?>
+    <a href="http://localhost/php/PHP/formulario_com_php.html">voltar</a>
+</body>
+</html>
