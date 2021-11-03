00b0fe591ea8a70a49a18bf0fdaa43af082ce0df
WilliamAraujo237 <william237.araujo@gmail.com>
2021-11-03 12:51:38 -0300

    Create Primeiros_calculos.php
    primeiros cálculos com php

---
+++ Primeiros_calculos.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="PT=BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiros Calculos</title>
+</head>
+
+<body>
+    <?php
+    $valor1=3;
+    $valor2=3;
+    $resultado=$valor1 + $valor2;
+    echo"a soma ente $valor1 e $valor2 é igual a $resultado";
+?>
+</body>
+</html>
